993b6901f8e0fdc5836bd5d66bb4814df86f86fb
kgothatso Phetla <146656433+seasoned-datascientist@users.noreply.github.com>
2025-03-05 09:33:36 +0200

    added files via load

---
+++ helper.py
@@ -0,0 +1,20 @@
+
+import matplotlib.pyplot as plt
+# colors for visualization
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+def plot_results(model, pil_img, results):
+    plt.figure(figsize=(16,10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    scores, labels, boxes = results["scores"], results["labels"], results["boxes"]
+    colors = COLORS * 100
+    for score, label, (xmin, ymin, xmax, ymax),c  in zip(scores.tolist(), labels.tolist(), boxes.tolist(), colors):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                   fill=False, color=c, linewidth=3))
+        text = f'{model.config.id2label[label]}: {score:0.2f}'
+        ax.text(xmin, ymin, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    plt.axis('off')
+    plt.show()
